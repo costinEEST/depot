6b8e191ba3e6160a7e7e80c67a45f874ed83653a
Constantin Câmpean <mail.la.costi@gmail.com>
2022-04-07 21:06:38 +0300

    remove the copyright message

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,14 +6,6 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-#---
-# Excerpted from "Agile Web Development with Rails 6",
-# published by The Pragmatic Bookshelf.
-# Copyrights apply to this code. It may not be used to create training material,
-# courses, books, articles, and the like. Contact us if you are in doubt.
-# We make no guarantees that this code is fit for any purpose.
-# Visit http://www.pragmaticprogrammer.com/titles/rails6 for more book information.
-#---
 # encoding: utf-8
 # Product.delete_all
 Product.create!(title: 'Docker for Rails Developers',
